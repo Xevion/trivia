368f6130ec26b08fd6fef209514a54a1055dcdbe
Xevion <xevioni@yandex.com>
2020-06-19 16:02:27 -0500

    rem config.py, basic scores data, api and basic routes

--- trivia/api.py
+++ trivia/api.py
@@ -3,3 +3,28 @@ api.py
 
 Handles backend routes assisting
 """
+from flask import request
+
+from trivia import app
+
+
+@app.route("/api/changed")
+@app.route("/api/changed/")
+def changed():
+    """
+    A simple substitute for the 304 Not Modified HTTP return.
+
+    :return True if data has changed since last
+
+    TODO: Remove this function once a proper 304 Not Modified implementation is found for client side.
+    """
+
+    time = request.args.get('last')
+
+
+@app.route("/api/refresh")
+@app.route("/api/refresh/")
+def refresh():
+    """
+    Used for refreshing client-side table data. Returns a JSON response with all data necessary to build the table.
+    """
--- trivia/routes.py
+++ trivia/routes.py
@@ -3,3 +3,15 @@ routes.py
 
 Handles user frontend routes.
 """
+
+from trivia import app
+
+
+@app.route("/")
+def index():
+    """
+    Handles the frontend user index.
+
+    :return:
+    """
+    return 'index'
