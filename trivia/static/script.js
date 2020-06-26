89b3acf5ab2d37f0f27f4ec2b15c1e5163863064
Xevion <xevioni@yandex.com>
2020-06-26 09:16:38 -0500

    update client to duplicate original project layout, basic button toggle figured out

---
+++ trivia/static/script.js
@@ -0,0 +1,62 @@
+// Client Constants
+const SCROLL_INTERVAL = 1500;
+const AUTOREFRESH_INTERVAL = 3000;
+
+// Client Variables
+let AUTOSCROLL = false;
+let AUTOREFRESH = false;
+
+// Client Timeouts
+let SCROLL_TIMEOUT;
+let REFRESH_TIMEOUT;
+
+// Scroll table once.
+function ScrollOnce(autoscroll) {
+    // If autoscroll has been disabled in between timer, this if statement will prevent it from executing.
+    if (AUTOSCROLL)
+        $(".team-row:first").appendTo("tbody")
+
+    // Restart autoscroll as needed.
+    if (autoscroll && AUTOSCROLL) {
+        setTimeout(function () {
+            ScrollOnce(true)
+        }, SCROLL_INTERVAL)
+    }
+}
+
+function ToggleAutoscroll() {
+    AUTOSCROLL = !AUTOSCROLL;
+
+    if (AUTOSCROLL) {
+        $(".js-scroll-row-start").hide();
+        $(".js-scroll-row-stop").show();
+    } else {
+        $(".js-scroll-row-start").show();
+        $(".js-scroll-row-stop").hide();
+    }
+}
+
+function ToggleAutorefresh() {
+    AUTOREFRESH = !AUTOREFRESH;
+
+    if (AUTOREFRESH) {
+        $(".js-refresh-start").hide();
+        $(".js-refresh-stop").show();
+    } else {
+        $(".js-refresh-start").show();
+        $(".js-refresh-stop").hide();
+    }
+}
+
+// Client Initialization
+$().ready(function () {
+    // Setup all click functions
+    $(".js-scroll-row-start").on("click", ToggleAutoscroll);
+    $(".js-scroll-row-stop").on("click", ToggleAutoscroll);
+
+    $(".js-refresh-start").on("click", ToggleAutorefresh);
+    $(".js-refresh-stop").on("click", ToggleAutorefresh).addClass("pointer");
+
+    ToggleAutoscroll()
+    ToggleAutorefresh()
+})
